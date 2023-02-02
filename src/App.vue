60a65cfd3a75b748e6cb5f749da6d79da95a222a
Horacio <horacio.schumann@gmail.com>
2023-02-02 10:30:11 -0300

    feat: added header as well as its initial styles

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,36 @@
 <template>
-  <h1>Hello</h1>
+  <div class="container">
+    <header>
+      <h1>Quizes</h1>
+      <input type="text" placeholder="Search..." />
+    </header>
+
+    <div class="options-container"></div>
+  </div>
 </template>
+
+<style scoped>
+.container {
+  max-width: 1000px;
+  margin: 0 auto;
+}
+
+header {
+  margin-bottom: 10px;
+  margin-top: 30px;
+  display: flex;
+  align-items: center;
+}
+
+header h1 {
+  font-weight: bold;
+  margin-right: 30px;
+}
+
+header input {
+  border: none;
+  background-color: rgba(128, 128, 128, 0.1);
+  padding: 10px;
+  border-radius: 5px;
+}
+</style>
