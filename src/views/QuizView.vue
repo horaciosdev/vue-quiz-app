827e1843998816cceb7dabdd1208ac61cf1631ca
Horacio <horacio.schumann@gmail.com>
2023-02-02 18:43:16 -0300

    feat: Parameters passed to the Question component and rendered as well.

--- src/views/QuizView.vue
+++ src/views/QuizView.vue
@@ -1,13 +1,24 @@
 <script setup>
 import Question from "../components/Question.vue";
 import QuizHeader from "../components/QuizHeader.vue";
+import { useRoute } from "vue-router";
+import { ref } from "vue";
+import quizes from "../data/quizes.json";
+
+const route = useRoute();
+
+const quizId = parseInt(route.params.id);
+
+const quiz = quizes.find((q) => q.id === parseInt(quizId));
+
+const currentQuestionIndex = ref(0);
 </script>
 
 <template>
   <div>
     <QuizHeader />
     <div>
-      <Question />
+      <Question :question="quiz.questions[currentQuestionIndex]"/>
     </div>
   </div>
 </template>
