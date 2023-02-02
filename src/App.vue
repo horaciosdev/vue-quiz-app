558b5876794fc398c203eeb4b84d25e329003d65
Horacio <horacio.schumann@gmail.com>
2023-02-02 11:03:50 -0300

    feat: added search functionality

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,22 @@
 <script setup>
 import q from "./data/quizes.json";
-import { ref } from "vue";
+import { ref, watch } from "vue";
 
 const quizes = ref(q);
+const search = ref("");
+
+watch(search, () => {
+  quizes.value = q.filter((quiz) =>
+    quiz.name.toLowerCase().includes(search.value.toLowerCase())
+  );
+});
 </script>
 
 <template>
   <div class="container">
     <header>
       <h1>Quizes</h1>
-      <input type="text" placeholder="Search..." />
+      <input v-model.trim="search" type="text" placeholder="Search..." />
     </header>
 
     <div class="options-container">
