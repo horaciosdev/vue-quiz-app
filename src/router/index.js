8cb60dc3a3697ec315de9bb9f3ff2e42c51abe2f
Horacio <horacio.schumann@gmail.com>
2023-02-02 17:51:50 -0300

    feat: vue-router installed and added to the project

---
+++ src/router/index.js
@@ -0,0 +1,15 @@
+import { createRouter, createWebHistory } from "vue-router";
+import QuizesView from "../views/QuizesView.vue";
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: "/",
+      name: "quizes",
+      component: QuizesView,
+    },
+  ],
+});
+
+export default router;
