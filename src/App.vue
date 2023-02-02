03db3b15e5be7321d5f3db63260b45722bb1bc3d
Horacio <horacio.schumann@gmail.com>
2023-02-02 12:05:28 -0300

    feat: isolated card in its own component.

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <script setup>
 import q from "./data/quizes.json";
 import { ref, watch } from "vue";
+import Card from "./components/Card.vue";
 
 const quizes = ref(q);
 const search = ref("");
@@ -18,15 +19,8 @@ watch(search, () => {
       <h1>Quizes</h1>
       <input v-model.trim="search" type="text" placeholder="Search..." />
     </header>
-
     <div class="options-container">
-      <div v-for="quiz in quizes" :key="quiz.id" class="card">
-        <img :src="quiz.img" alt="" />
-        <div class="card-text">
-          <h2>{{ quiz.name }}</h2>
-          <p>{{ quiz.questions.length }} questions</p>
-        </div>
-      </div>
+      <Card v-for="quiz in quizes" :key="quiz.id" :quiz="quiz" />
     </div>
   </div>
 </template>
@@ -61,29 +55,9 @@ header input {
   flex-wrap: wrap;
   margin-top: 40px;
 }
-
-/* CARD */
-.card {
-  width: 310px;
-  overflow: hidden;
-  border-radius: 2%;
-  box-shadow: 0px 0px 10px rgba(255, 255, 255, 0.1);
-  margin-bottom: 35px;
-  margin-right: 20px;
-  cursor: pointer;
-}
-
-.card img {
-  width: 100%;
-  height: 190px;
-  margin: 0;
-}
-
-.card .card-text {
-  padding: 0 5px;
-}
-
-.card .card-text h2 {
-  font-weight: bold;
-}
 </style>
+
+<!-- 
+  I stop in 3:14:33
+  https://youtu.be/I_xLMmNeLDY?t=11673 
+-->
